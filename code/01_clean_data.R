9d34dfeca34456b6acac8e61631207f79f5f4b75
L.B. Baptist <l.b.baptist@emory.edu>
2026-04-14 20:22:18 -0400

    checked clean data script

--- code/01_clean_data.R
+++ code/01_clean_data.R
@@ -8,8 +8,12 @@ head(data)
 
 clean_data <- data %>%
   select(-any_of(c("USMER", "MEDICAL_UNIT", "PREGNANT"))) %>%
-  na.omit() %>%
-  rename(DEATH = DATE_DIED)
+  rename(DEATH = DATE_DIED) %>%
+  mutate(
+    DEATH = ifelse(is.na(DEATH), "No", "Yes"),
+    DEATH = factor(DEATH, levels = c("No", "Yes"))
+  ) %>%
+  na.omit()
 dim(clean_data)
 # Save cleaned dataset
 saveRDS(clean_data, "data/clean_data.rds")
